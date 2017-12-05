0cbef7634882edf6a38a22ec7924e00da14fbbb7
adrien <adrien.bardet@phelma.grenoble-inp.fr>
2017-12-05 01:32:29 +0100

    add loss function and training loop test

--- src/VAE.py
+++ src/VAE.py
@@ -15,7 +15,7 @@ from EncoderDecoder import Encoder, Decoder
 
 class VAE(nn.Module):
 
-    def __init__(self, X_dim, Z_dim, IOh_dims_Enc, IOh_dims_Dec, NL_types_Enc, NL_types_Dec, mb_size=64):
+    def __init__(self, X_dim, Z_dim, IOh_dims_Enc, IOh_dims_Dec, NL_types_Enc, NL_types_Dec, mb_size=64, beta=1):
 
         # superclass init
         super(VAE, self).__init__()
@@ -61,7 +61,7 @@ class VAE(nn.Module):
         self.lr = 1e-3
         # minibacth size
         self.mb_size = mb_size
-        self.beta = 0
+        self.beta = beta
 
         self.z_mu = None
         self.z_logSigma = None
@@ -75,7 +75,8 @@ class VAE(nn.Module):
             print "ERROR_VAE_forward: VAE not correctly created"
             return None
         # compute z from X
-        self.z = self.encode(X)
+        # the size -1 is inferred from other dimensions
+        self.z = self.encode(X.view(-1, self.encoder.dimX))
         # compute X_sample from z
         self.X_sample = self.decode(self.z)
         return self.X_sample
@@ -86,33 +87,41 @@ class VAE(nn.Module):
         # then var_h goes through deeper layers
         for i in range(self.encoder.nb_h - 1):
             var_h = getattr(F, self.NL_funcE[
-                            i + 1])(self.encoder.h_layers[i + 1](X))
+                            i + 1])(self.encoder.h_layers[i + 1](var_h))
 
         # get z's mu and logSigma
         self.z_mu = self.encoder.h_mu(var_h)
         self.z_logSigma = self.encoder.h_logSigma(var_h)
 
         # reparametrization trick
-        return self.reparametrize(self.z_mu, self.z_logSigma)
+        return self.reparametrize()
 
     def decode(self, z):
         # first layer takes z in input
         var_h = getattr(F, self.NL_funcD[0])(self.decoder.h_layers[0](z))
         # then var_h goes through deeper layers
         for i in range(self.decoder.nb_h - 1):
-            var_h = getattr(F, self.NL_funcD[i+1])(
-                self.decoder.h_layers[i+1](var_h))
+            var_h = getattr(F, self.NL_funcD[i + 1])(
+                self.decoder.h_layers[i + 1](var_h))
 
         return var_h
 
-    def reparametrize(self, mu, logSigma):
+    def reparametrize(self):
         # eps = Variable(torch.randn(self.mb_size, self.encoder.dimZ))
-        # return mu + torch.exp(logSigma / 2) * eps
-        std = logSigma.mul(0.5).exp_()
+        # return self.z_mu + torch.exp(self.z_logSigma / 2) * eps
+        std = self.z_logSigma.mul(0.5).exp_()
         eps = Variable(std.data.new(std.size()).normal_())
-        return eps.mul(std).add_(mu)
+        return eps.mul(std).add_(self.z_mu)
 
-        # def loss(self,X_sample, X, z_mu, z_logSigma):
+    def loss(self, X):
+        bce = F.binary_cross_entropy(
+            self.X_sample, X.view(-1, self.encoder.dimX))
+        kld = - 0.5 * torch.sum(1 + self.z_logSigma -
+                                self.z_mu.pow(2) - self.z_logSigma.exp())
+        # Normalise by same number of elements as in reconstruction
+        kld /= self.mb_size * self.encoder.dimX
+        loss = bce + self.beta * kld
+        return loss
 
         # def save(self, name):
 
--- unitTest/VAETest.py
+++ unitTest/VAETest.py
@@ -3,6 +3,9 @@ import sys
 # Add the src folder path to the sys.path list
 sys.path.append('../src')
 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.image as mpimg
 import numpy
 import tensorflow
 from tensorflow.examples.tutorials.mnist import input_data
@@ -11,6 +14,9 @@ from torch import optim
 from torch.autograd import Variable
 
 from VAE import VAE
+
+
+mnist = input_data.read_data_sets('../MNIST_data', one_hot=True)
 
 
 class TestVAECreation(unittest.TestCase):
@@ -96,10 +102,9 @@ class TestVAECreation(unittest.TestCase):
 class TestVAEFunctions(unittest.TestCase):
 
     def test_VAE_lonelyForward(self):
-        mnist = input_data.read_data_sets('../MNIST_data', one_hot=True)
         mb_size = 64
         X, _ = mnist.train.next_batch(mb_size)
-        X = Variable(torch.from_numpy(X))
+        X = Variable(torch.from_numpy(X), volatile=True)
         X_dim = mnist.train.images.shape[1]
         Z_dim = 40
         IOh_dims_Enc = [X_dim, 400, Z_dim]
@@ -111,13 +116,97 @@ class TestVAEFunctions(unittest.TestCase):
 
         optimizer = optim.Adam(vae.parameters(), lr=1e-3)
         optimizer.zero_grad()
-        out = 0
+        out = None
         out = vae(X)
         vae.encoder.getInfo()
         vae.decoder.getInfo()
-        # print(out)
         self.assertTrue((vae.created) and (
             out.size()[1] == X_dim and out.size()[0] == mb_size))
+
+    def test_VAE_Learning(self):
+        mb_size = 1
+        X, _ = mnist.train.next_batch(mb_size)
+
+        X = Variable(torch.from_numpy(X))
+        X_dim = mnist.train.images.shape[1]
+        Z_dim = 1
+        IOh_dims_Enc = [X_dim, 50, Z_dim]
+        IOh_dims_Dec = [Z_dim, 50, X_dim]
+        NL_types_Enc = ['relu']
+        NL_types_Dec = ['relu', 'sigmoid']
+        vae = VAE(X_dim, Z_dim, IOh_dims_Enc,
+                  IOh_dims_Dec, NL_types_Enc, NL_types_Dec, mb_size)
+
+        optimizer = optim.Adam(vae.parameters(), lr=1e-3)
+
+        fig = plt.figure()
+        ims = []
+
+        for i in range(150):
+            optimizer.zero_grad()
+            out = vae(X)
+            loss = vae.loss(X)
+            if i == 0:
+                initialLoss = loss.data[0]
+            print("Loss -> " + str(loss.data[0]))
+            loss.backward()
+            optimizer.step()
+
+            # update plot
+            gen = out.data.numpy()
+            gen_2D = numpy.reshape(gen[0], (28, 28)) * 255
+            im = plt.imshow(gen_2D, animated=True)
+            ims.append([im])
+
+        ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+                                        repeat_delay=1000)
+
+        plt.show()
+
+        self.assertTrue((vae.created) and (loss.data[0] < initialLoss))
+
+    # experimental
+    # def test_VAE_LearningBigger(self):
+    #     mb_size = 1
+
+    #     #create X from X_2D
+    #     X_2D = mpimg.imread('../../../stinkbug.png')
+    #     # X = numpy.reshape(X_2D, (1, 500*375)) / 255
+    #     X = X_2D
+
+    #     X = Variable(torch.from_numpy(X))
+    #     X_dim = 500*375
+    #     Z_dim = 20
+    #     IOh_dims_Enc = [X_dim, 40, Z_dim]
+    #     IOh_dims_Dec = [Z_dim, 40, X_dim]
+    #     NL_types_Enc = ['relu']
+    #     NL_types_Dec = ['relu', 'sigmoid']
+    #     vae = VAE(X_dim, Z_dim, IOh_dims_Enc,
+    #               IOh_dims_Dec, NL_types_Enc, NL_types_Dec, mb_size)
+
+    #     optimizer = optim.Adam(vae.parameters(), lr=1e-3)
+
+    #     fig = plt.figure()
+    #     ims = []
+
+    #     for i in range(400):
+    #         optimizer.zero_grad()
+    #         out = vae(X)
+    #         loss = vae.loss(X)
+    #         print("Loss -> " + str(loss.data))
+    #         loss.backward()
+    #         optimizer.step()
+
+    #         #update plot
+    #         gen = out.data.numpy()
+    #         gen_2D = numpy.reshape(gen[0], (375, 500))
+    #         im = plt.imshow(gen_2D, animated=True)
+    #         ims.append([im])
+
+    #     ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+    #                             repeat_delay=1000)
+
+    #     plt.show()
 
 suiteEncoder = unittest.TestLoader().loadTestsFromTestCase(TestVAECreation)
 print "\n\n------------------- VAE Creation Test Suite -------------------\n"
