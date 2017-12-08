fb07cb41bbf70bd0e9f049cac5d1d23376fc663d
bavovk <bavovankerrebroeck@gmail.com>
2017-12-08 11:19:14 +0100

    Visualization pour Toydataset
    Updated the script to work with Toydataset (spectre 1024) instead of NMIST

--- src/VisualizeScript.py
+++ src/VisualizeScript.py
@@ -1,114 +1,50 @@
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 19 14:26:00 2017
+Created on Wed Dec  6 15:36:40 2017
 
 @author: bavo
 """
-import matplotlib.gridspec as gridspec
-import matplotlib.pyplot as plt
-import torch
-import numpy as np
-import torchvision
-from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as transforms
 import os
-os.chdir("/home/bavo/Documents/ATIAM/4_Informatique/MachineLearning_Project")
+os.chdir('/home/bavo/Documents/ATIAM/4_Informatique/MachineLearning_Project/1_VAE_model/')
 
-class VAE_Vanilla_test(nn.Module):
-    
-    mb_size = 400
-    Z_dim = 2
-    h_dim = 128
-    X_dim = 784     # =28*28 
-    lr = 1e-3
-    
-    def __init__(self):
-        super(VAE_Vanilla_test, self).__init__()
-        
-        self.Q_in  = nn.Linear(784,128)
-        self.Q_mu  = nn.Linear(128,VAE_Vanilla_test.Z_dim)
-        self.Q_var = nn.Linear(128,VAE_Vanilla_test.Z_dim)
-        
-        self.P_z   = nn.Linear(VAE_Vanilla_test.Z_dim,128)
-        self.P_out = nn.Linear(128,784)
-    
-    def forward(self, X):
-        #       Q        
-        h = F.relu(self.Q_in(X))
-        z_mu = self.Q_mu(h)
-        z_var = self.Q_var(h)
-        #       Sample
-        z = self.sample_z(z_mu, z_var)
-        #       P
-        X_sample = self.P(z)
-        #       Loss
-        loss = self.Loss(X_sample, z_mu, z_var)
-        return X_sample
-    def Q(self,X):
-        #       Q        
-        h = F.relu(self.Q_in(X))
-        z_mu = self.Q_mu(h)
-        z_var = self.Q_var(h)
-        return z_mu, z_var
+#%%
+import torch
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import VAE_model
+reload(VAE_model)
 
-    def sample_z(self, mu, log_var):
-        eps = Variable(torch.randn(self.mb_size, self.Z_dim))
-        return mu + torch.exp(log_var / 2) * eps
-    
-    def P(self,z):
-        h = F.relu(self.P_z(z))
-        X = F.sigmoid(self.P_out(h))
-        return X
-    
-    def Loss(self,X_sample, z_mu, z_var):
-        recon_loss = F.binary_cross_entropy(X_sample, X, size_average=False) / VAE_test.mb_size
-        kl_loss = torch.mean(0.5 * torch.sum(torch.exp(z_var) + z_mu**2 - 1. - z_var, 1))
-        loss = recon_loss + kl_loss
-        return loss
-        
-def plot_images(samples):
-    fig = plt.figure(figsize=(4,4))
-    gs = gridspec.GridSpec(20,20)
-    gs.update(wspace=0.05, hspace=0.05)
-    for i, sample in enumerate(samples):
-        ax = plt.subplot(gs[i])
-        plt.axis('off')
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.set_aspect('equal')
-        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
-    plt.plot()
+# Load Model
+VAE_test = VAE_model.VAE_Vanilla()
+VAE_test.load_state_dict(torch.load('model_2_Toy1_2', map_location={'cuda:0': 'cpu'}))
 
-#%%                                     Load model
-VAE_test = VAE_Vanilla_test()
-#VAE_test.load_state_dict(torch.load('./model_100'))
-VAE_test.load_state_dict(torch.load('model_2_warmup', map_location={'cuda:0': 'cpu'}))
-       
-#%%                                     Load data
-trainset = torchvision.datasets.MNIST(root='./data', train=True,download=False, transform=transforms.ToTensor())
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=VAE_test.mb_size,shuffle=False, num_workers=0)
+# Load data
+toydataset_1 = VAE_model.ToyDataset_1(npz_file='toy_dataset_2b_BVK.npz', 
+                            root_dir='/home/bavo/Documents/ATIAM/4_Informatique/MachineLearning_Project/2_VAE_dataset/')
+trainloader = torch.utils.data.DataLoader(toydataset_1, batch_size=VAE_test.mb_size, shuffle=True, num_workers=4)
+
 dataiter = iter(trainloader)
-image_in, labels = dataiter.next()
-X = image_in.view(VAE_test.mb_size,VAE_test.X_dim)
+sample_dict = dataiter.next()
+X = sample_dict['image']
 X = Variable(X)
+X = X.float()
+loss, X_sample = VAE_test.forward(X)
 
 #%%                              Test 1: compare in- and output
-X_sample = VAE_test.forward(X)
-image_out = X_sample[0].data.numpy()
-image_out = image_out.reshape(VAE_test.mb_size,28,28)
-image_in = torch.squeeze(image_in)
-image_in = image_in.numpy()
+idx = 90
+image_out = X_sample[idx].data.numpy()
+image_in = X[idx].data.numpy()
 images = [image_in, image_out]
 
 # Plot
-for j in range(2):
-    fig = plt.figure(figsize=(5,5))
-    gs = gridspec.GridSpec(np.sqrt(VAE_test.mb_size),np.sqrt(VAE_test.mb_size))
-    gs.update(wspace=0.05, hspace=0.05)
-    samples = images[j]
-    plot_images(samples)
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax1.plot(images[0])
+ax2 = fig.add_subplot(212)
+ax2.plot(images[1])
 
 #%%                             Test 2: analyse z-space (random z)
 z = Variable(torch.randn(VAE_test.mb_size, VAE_test.Z_dim))
@@ -116,14 +52,51 @@ z = Variable(torch.randn(VAE_test.mb_size, VAE_test.Z_dim))
 samples = VAE_test.P(z).data.numpy()
 
 # Plot
-plot_images(samples)
+idx = 0
+fig = plt.figure()
+ax1 = fig.add_subplot(411)
+ax1.plot(samples[idx])
+ax2 = fig.add_subplot(412)
+ax2.plot(samples[idx+1])
+ax2 = fig.add_subplot(413)
+ax2.plot(samples[idx+2])
+ax2 = fig.add_subplot(414)
+ax2.plot(samples[idx+3])
 
-# plot one z
-temp = torch.from_numpy(np.array([-2.0,-2.0]))
-temp = Variable(temp.float())
-temp2 = VAE_test.P(temp).data.numpy()
-plt.imshow(temp2.reshape(28,28))
+#%%                                     Signal Reconstruction
 
+def griffLim1(S): # Griffin-Lim algorithm for signal reconstruction
+    Nfft = S.shape[0]
+    S = np.log1p(S) # ln(1 + S)
+    a = np.exp(S) - 1
+    p = 2*np.pi*np.random.random_sample(a.shape) - np.pi # Init random phase
+    for i in xrange(250): # Iterate to approximate phase
+        S = a*np.exp(1j*p)
+        x = np.fft.ifft(S,Nfft)
+        p = np.angle(np.fft.fft(x,Nfft))
+    return np.real(x)
+
+def griffLim2(S): # Griffin-Lim algorithm for signal reconstruction
+    Nfft = S.shape[0]
+    S = np.log1p(S) # ln(1 + S)
+    a = np.exp(S) - 1
+    p = 2*np.pi*np.random.random_sample(a.shape) - np.pi # Init random phase
+    S0 = np.sqrt(S)
+    for i in xrange(250): # Iterate to approximate phase
+        S = a*np.exp(1j*p)
+        Si = np.linalg.norm(S)
+        x = np.fft.ifft(S0*S/Si,Nfft)
+        p = np.angle(np.fft.fft(x,Nfft))
+    return np.real(x)
+
+samples_recon = 10**(samples/20)
+#spectrum = np.array([[0], samples_recon[0,:]])
+
+sig1 = griffLim1(samples_recon[0,:])
+sig2 = griffLim2(samples_recon[0,:])
+
+D = np.fft.rfft(sig1,2024)
+log_D = 20*np.log10(np.abs(D)**2)
 #%%                           Test 3: analyse z=space (Guassian mesh)
 from scipy.stats import norm
 
@@ -142,58 +115,10 @@ z = Variable(torch.from_numpy(z).float())
 samples = VAE_test.P(z).data.numpy()
 
 # Plot
-plot_images(samples)
-
-#%%                         Visualize (PCA)
-from sklearn.decomposition import PCA as sklearnPCA
-from mpl_toolkits.mplot3d import Axes3D
-
-size = 800
-image_size = 28*28
-n_components = 2
-
-trainset = torchvision.datasets.MNIST(root='./data', train=True,download=False, transform=transforms.ToTensor())
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=size,shuffle=False, num_workers=0)
-dataiter = iter(trainloader)
-image_in, labels = dataiter.next()
-X = image_in.view(size,image_size)
-X = Variable(X)
-
-data_in = image_in.view(size, image_size).numpy()
-labels_in = labels.numpy()
-
-VAE_test = VAE_Vanilla_test()
-
-z_mu, z_var = VAE_test.Q(X)
-z_mu = z_mu.data.numpy()
-z_var = z_var.data.numpy()
-
-sklearn_pca = sklearnPCA(n_components)
-#PCA_proj = z_mu
-#PCA_proj = sklearn_pca.fit_transform(data_in)
-PCA_proj = sklearn_pca.fit_transform(z_mu)
-
-fig = plt.figure(figsize=(12, 8))
-#ax = Axes3D(fig)
-ax = fig.add_subplot(111)
-for i in range(10):
-    bool_ar = labels_in == i
-    color_ar = np.random.rand(3,1)
-    for j in range(len(PCA_proj[bool_ar,0])):
-        x = PCA_proj[bool_ar, 0][j]
-        y = PCA_proj[bool_ar, 1][j]
-#        z = PCA_proj[bool_ar, 2][j]
-        ax.text(x, y, str(i), bbox=dict(color = color_ar, alpha=0.5), fontsize=12)
-add_x = np.std(PCA_proj[:,0])/2
-add_y = np.std(PCA_proj[:,1])/2
-#add_z = np.std(PCA_proj[:,2])/2
-ax.set_xlim(min(PCA_proj[:,0])-add_x ,max(PCA_proj[:,0])+add_x )
-ax.set_ylim(min(PCA_proj[:,1])-add_y,max(PCA_proj[:,1])+add_y)
-#ax.set_zlim(min(PCA_proj[:,2])-add_z,max(PCA_proj[:,2])+add_z)
-
-plt.show()
-
-
-
-
-
+fig = plt.figure()
+for i in range(1,100):
+    ax = fig.add_subplot(10,10,i)
+    ax.plot(samples[i,:])
+fig.show()
+    
+    
